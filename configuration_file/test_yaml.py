6cd91ffa997269629a810e0d2c58f2a766abbf6d
The Noart <101931052+LIKE9426334946@users.noreply.github.com>
2026-03-07 11:22:25 +0800

    Update test_yaml.py

--- configuration_file/test_yaml.py
+++ configuration_file/test_yaml.py
@@ -1,4 +1,13 @@
-#读取yaml文件并打印值
+# 读取yaml文件并打印值
+
+# 以下注释代码放在notebook中，可以直接运行
+"""
+%cd /kaggle/working
+!rm -rf /kaggle/working/sandbox
+!git clone https://github.com/LIKE9426334946/sandbox.git
+%cd /kaggle/working/sandbox/configuration_file
+!python3 test_yaml.py
+"""
 import yaml
 
 
